c1c9d4c78898867a264a8d722ecd002de1cbb98f
Metin Yalcinkaya <metinyalcinkaya@hotmail.com.au>
2026-05-07 19:16:25 +1000

    fish: path as globals instead of universal

--- fish/.config/fish/conf.d/00-env.fish
+++ fish/.config/fish/conf.d/00-env.fish
@@ -3,7 +3,7 @@ set -gx VISUAL nvim
 set -gx MANPAGER "nvim +Man!"
 set -gx DOTFILES $HOME/.dotfiles
 
-fish_add_path $HOME/.cargo/bin
-fish_add_path $HOME/go/bin
-fish_add_path /opt/homebrew/bin
-fish_add_path $HOME/Library/Python/3.9/bin
+fish_add_path -g $HOME/.cargo/bin
+fish_add_path -g $HOME/go/bin
+fish_add_path -g /opt/homebrew/bin
+fish_add_path -g $HOME/Library/Python/3.9/bin
